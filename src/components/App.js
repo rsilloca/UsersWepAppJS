8264c05e6b7a797aa1919109df4ff88f9685ec07
Raquel Silloca <rsilloca@unsa.edu.pe>
2022-06-28 16:22:20 -0500

    Maquetado y estilos user modal completos

--- src/components/App.js
+++ src/components/App.js
@@ -9,8 +9,22 @@ function loadData() {
             users.forEach(user => {
                 let userCard = new UserCard();
                 userCard.setAttribute('user', JSON.stringify(user));
+                userCard.onclick = () => showUserModal(user);
                 containerUserList.appendChild(userCard);
             });
         });
     });
+}
+
+function showUserModal(user) {
+    let userModal = new UserModalDetail();
+    userModal.setAttribute('user', JSON.stringify(user));
+    userModal.setAttribute('id', 'user-modal-detail');
+    let container = document.getElementById('container');
+    container.appendChild(userModal);
+}
+
+function closeUserModal() {
+    let modal = document.getElementById('user-modal-detail');
+    modal.parentNode.removeChild(modal);
 }
